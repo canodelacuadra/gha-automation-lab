f6b2964ec12f8e1d53773036ab45f7c4ae9a70bc
canodelacuadra <canodelacuadra@gmail.com>
2026-02-09 15:49:58 +0100

    arreglo base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gha-automation-lab/',  // <--- para que cargue scripts y assetes
   plugins: [react()],
 })
